49225d7b9b0c39d23aaef2062a107c7e31abfa39
=nzitindafustino <=nzitindafustino@gmail.com>
2020-11-19 14:59:01 +0200

    scroll app

---
+++ app/src/main/java/com/example/text_and_scrolling_views_217163378/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.text_and_scrolling_views_217163378;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
